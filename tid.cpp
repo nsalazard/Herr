0c77a8060a5d7ab1abccb60caee2f1322295ff94
nsalazard <nsalazard@unal.edu.co>
2021-05-10 10:25:18 -0500

    Tid

--- tid.cpp
+++ tid.cpp
@@ -1,25 +1,27 @@
-   #include <omp.h>
-    #include <cstdio.h>
+#include <omp.h>
+#include <cstdio.h>
 
-    int main(int argc, char *argv[]) {
-  
-      int nthreads, tid;
-  
-      /* Fork a team of threads with each thread having a private tid variable */
-    #pragma omp parallel private(tid)
-      {
-    
-        /* Obtain and print thread id */
-        tid = omp_get_thread_num();
-        std::printf("Hello World from thread = %d\n", tid);
-    
-        /* Only master thread does this */
-        if (tid == 0) 
+int main(int argc, char *argv[]) {
+
+    int nthreads, tid;
+
+    /* Fork a team of threads with each thread having a private tid variable */
+#pragma omp parallel private(tid)
     {
-      nthreads = omp_get_num_threads();
-      std::printf("Number of threads = %d\n", nthreads);
-    }
-    
-      }  /* All threads join master thread and terminate */
-  
-    }
+
+    /* Obtain and print thread id */
+    tid = omp_get_thread_num();
+    std::printf("Hello World from thread = %d\n", tid);
+
+    /* Only master thread does this */
+    if (tid == 0) 
+{
+    nthreads = omp_get_num_threads();
+    std::printf("Number of threads = %d\n", nthreads);
+}
+
+    }  /* All threads join master thread and terminate */
+     nthreads = omp_get_num_threads();
+    std::printf("Number of threads = %d\n", nthreads);
+
+}

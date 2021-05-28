4ccf91c939cdfdf4e0f7a8039264e8a00097651c
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-05-28 10:49:24 -0500

    gr

--- openMPI/example01.cpp
+++ openMPI/example01.cpp
@@ -10,7 +10,8 @@ int main(int argc, char **argv)
 
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
-
+  
+  MPI_Barrier (MPI_COMM_WORLD);
   for (int id = 0; id < np; id++) {
     if (id == pid) 
       std::cout << "Hello from pid: " << pid << " out of np: " << np << "\n";

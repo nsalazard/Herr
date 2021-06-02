d36e3ee15d1949087b2d7f4b225e071a3ebaa490
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-06-02 12:23:20 -0500

    sgr

--- openMPI/example01.cpp
+++ openMPI/example01.cpp
@@ -7,14 +7,28 @@ int main(int argc, char **argv)
 
   int pid;                 /* rank of process */
   int np;                 /* number of processes */
+  
+  int N = std::atoi(argv[1]);
 
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
   
-  for (int id = 0; id < np; id++) {
-    MPI_Barrier (MPI_COMM_WORLD);
-    if (id == pid) 
-      std::cout << "Hello from pid: " << pid << " out of np: " << np << "\n";
+  double Nc = 0;
+  int tag = 0;
+  if (pid != 0) {
+    // enviar Nc a pid 
+    MPI_Send(&Nc, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+    MPI_Recv(&Nc, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  } else { // pid ==0
+    // recibir de pid 1, 2, 3, 4, 5, ... , np-1
+      MPI_Recv(&Nc, 1, MPI_INT, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      Nc += 1;
+      MPI_Send(&Nc, 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
+      
+    // imprimir
+  }
+  if (0 == pid) {
+    std::cout << Nc << std::endl;
   }
   
   MPI_Finalize(); /* Mandatory */

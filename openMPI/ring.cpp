513e2c2ab4692c1f060be7054cf895a0c9ead71b
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-06-04 12:04:11 -0500

    dfd

--- openMPI/ring.cpp
+++ openMPI/ring.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char **argv)
 {
   MPI_Init(&argc, &argv);
-  int pid = 0, nproc = 0;
+  int pid = 0, np = 0;
 
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);

61e99d44f1c364f464af4b2798acd4c8917736da
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-06-04 09:28:12 -0500

    ahh

---
+++ openMPI/mpi_integ_reduc.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include "mpi.h"
+
+double f (double x){
+	return x*x;
+}
+
+//Parameters
+const int numberRects = 5000;
+const double xmin = 2.0;
+const double xmax = 5.0;
+
+int main(int argc, char **argv)
+{
+	int ii;
+	double area,at,height, width;
+
+	int pid,np;
+  MPI_Init(&argc, &argv); /* Mandatory */
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+
+	int nrect =numberRects/np;
+	int imin = pid * nrect;
+
+// MPI TIMER
+	double tstart = MPI_Wtime();
+	area = 0.0;
+	width = (xmax -xmin)/numberRects;
+
+	for(ii= imin; ii < imin + nrect; ++ii){
+		at = xmin + ii *width + width/2.0;
+		height = f(at);
+		area = area + width * height;
+	}
+double tend = MPI_Wtime();
+std::printf("Time from pid %d: %lf \n", pid, tend-tstart);
+//std::printf("The area from %lf to %lf is: %le \n", xmin + (imin*width),xmin + (imin*nrect)*width, area);
+
+int total = 0;
+MPI_Reduce(&area,&total,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+if(0 ==pid){
+  std::printf ("El area total es %f \n", total);
+  }
+
+ MPI_Finalize(); /* Mandatory */
+ return 0;
+}

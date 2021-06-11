b1a6613c2da76ed12fc0391c8fe54abeed2ce7be
nsalazard <61853949+nsalazard@users.noreply.github.com>
2021-06-11 09:22:59 -0500

    ahh

---
+++ array-mean-mp-mpi.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <chrono>
+#include <omp.h>
+#include "mpi.h"
+
+template <typename t1, typename t2>
+void print_elapsed(t1 start, t2 end );
+void fill(double * data, int n, int nthreads);
+double average_mp(double * d, int n, int nthreads);
+int average_mpi(double *data, int N, int pid, int np);
+void comm(int N, int Nc, int pid, int np);
+
+int main(int argc, char **argv)
+{
+  int N = std::atoi(argv[1]);
+  int NTH = std::atoi(argv[2]);
+  double * data = new double [N]; 
+
+  fill(data, N, NTH);
+
+  //average
+  auto start = std::chrono::steady_clock::now();
+  double avg = average_mp(data, N, NTH);
+  auto end = std::chrono::steady_clock::now();
+  std::cout << "Time-OpenMP: \t";
+  print_elapsed(start, end);
+
+  std::cout << "Average: " << avg << std::endl;
+  
+  MPI_Init(&argc, &argv); /* Mandatory */
+
+  int pid;                 /* rank of process */
+  int np;                 /* number of processes */
+  MPI_Comm_size(MPI_COMM_WORLD, &np);
+  MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+  double t0 = MPI_Wtime();
+  int Nc = average_mpi(data, N, pid, np);
+  comm(N, Nc, pid, np);
+  double t1 = MPI_Wtime();
+  std::cout << "TIME Open-MPI: " << t1-t0 << std::endl;
+  
+  MPI_Finalize();
+ 
+  delete [] data;
+  return 0;
+}
+
+void fill(double * d, int n, int nthreads)
+{
+#pragma omp parallel for num_threads (nthreads)
+  for (int ii = 0; ii < n; ++ii) {
+      d[ii] = 2*std::sin(ii) + std::log(ii + 1);
+    }
+}
+
+double average_mp(double * data, int n, int nthreads)
+{
+  double sum = 0.0;
+#pragma omp parallel for num_threads (nthreads) reduction(+:sum)
+  for (int ii = 0; ii < n; ++ii) {
+    sum += data[ii]; 
+  }
+  return sum/n;
+}
+
+void comm(int N, int Nc, int pid, int np)
+{
+  int tag = 0;
+  if (pid != 0) {
+    // enviar Nc a pid 0
+    int dest = 0;
+    MPI_Send(&Nc, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+  } else { // pid ==0
+    // recibir de pid 1, 2, 3, 4, 5, ... , np-1
+    double total = Nc;
+    for (int ipid = 1; ipid < np; ++ipid) {
+      MPI_Recv(&Nc, 1, MPI_INT, ipid, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      total += Nc;
+    }
+    // imprimir
+    total = total/N; 
+    std::cout << "Average OpenMPI: " << total << std::endl;
+  }
+}
+int average_mpi(double *data, int N, int pid, int np)
+{
+  int num = N/np;  
+  int imin = pid*num;
+  int imax = (pid+1)*num;
+  double total = 0;
+    for (int ii = imin; ii < imax; ++ii) {
+     total += data[ii];
+  }
+  return total;
+}
+
+template <typename t1, typename t2>
+void print_elapsed(t1 start, t2 end )
+{
+  std::cout.precision(6); std::cout.setf(std::ios::scientific);
+  std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()/1000.0 << "\n";
+}
